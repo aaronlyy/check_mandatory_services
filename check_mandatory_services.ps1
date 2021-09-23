47266587819cafba7c82f438a69351c2b539a962
aaronlyy <aaronlevican@gmail.com>
2021-09-23 16:14:27 +0200

    added comments, added parameter support

--- check_mandatory_services.ps1
+++ check_mandatory_services.ps1
@@ -1,22 +1,41 @@
+#* check_mandatory_services.ps1
+#* https://github.com/aaronlyy/check_mandatory_services
+#* Last update: 23.09.2021
+
+#Region Parameters
+Param(
+    [String]$SnapshotFilePath = "$PSScriptRoot/services.json",
+    [String]$LogFilePath = "$PSScriptRoot/mansvc.log"
+)
+#Endregion
+
+
+#* --- Start-Checking ------------------------------------------------------------------------
+#* Takes a file path to a .json file containing a list of services                           -
+#* Checks if every service in the list is currently running, if not serivce will be started. -
+#* Returns 0 if every service could be started or every service was already running          -
+#* Returns 2 if an error occured during checking                                             -
+#* -------------------------------------------------------------------------------------------
+#Region Start-Checking
 Function Start-Checking {
-    $services = Read-Config
+    $services = Read-Snapshot $SnapshotFilePath
     $errors = @()
 
     ForEach ($s in $Services){ # iter trough services
         $serv = Get-Service -Name $s # get service object from name
-        if ($serv.Status -ne "Running") { # check if service status is anything but running
+        if ($serv.Status -Ne "Running") { # check if service status is anything but running
             try {
                 Start-Service -Name $s -ErrorAction Stop # try to start the service
-                Write-Log "Service $s started"
+                Write-Log -Content "Service $s started" -FilePath $LogFilePath
             }
             catch {
                 $errors += $s # save name if starting did not work
-                Write-Log "Error starting service $s"
+                Write-Log -Content "Error starting service $s" -FilePath $LogFilePath
             }
         }
     }
 
-    if ($errors.Count -eq 0){
+    if ($errors.Count -Eq 0){
         Write-Host "All services are running"
         return 0
     }
@@ -25,36 +44,71 @@ Function Start-Checking {
         return 2
     }
 }
+#Endregion
 
 
-Function New-Config {
+#* --- New-Snapshot ----------------------------------------------------------------------------
+#* This function takes a filename and creates a new <filename>.json file with all currently    -
+#* running services.                                                                           -
+#* ---------------------------------------------------------------------------------------------
+#Region New-Snapshot
+Function New-Snapshot {
+    Param(
+        [Parameter(Mandatory=$True)]
+        [String]$FilePath
+    )
+
     $services = @()
     Get-Service | Where-Object {$_.Status -Eq "Running"} | ForEach { $services += $_.Name }
-    ConvertTo-Json $services | Out-File -FilePath "$PSScriptRoot/services.json"
+    ConvertTo-Json $services | Out-File -FilePath $FilePath
 }
+#Endregion
 
 
-Function Read-Config {
-    $services = Get-Content "$PSScriptRoot/services.json" | ConvertFrom-Json
+#* --- Read-Snapshot ----------------------------------------------------------------------------
+#* This function takes a filename and parses the JSON to an array of all running services       -
+#* Returns the array.                                                                           -
+#* ----------------------------------------------------------------------------------------------
+#Region Read-Snapshot
+Function Read-Snapshot {
+
+    Param(
+    [Parameter(Mandatory=$True)]
+    [String]$FilePath
+    )
+
+    $services = Get-Content $FilePath | ConvertFrom-Json
     return $services
 }
+#Endregion
 
+#* --- Write-Log -------------------------------------------------------------------
+#* This function takes content and a filepath and writes it to a file              -
+#* ---------------------------------------------------------------------------------
+#Region Write-Log
 Function Write-Log {
     Param
     (
-        [String]
-        $text
-    )
-    "[$(Get-Date -format "yyyy-MM-dd HH:mm:ss")] $text" | Out-File "$PSScriptRoot/mansvc.log" -Append
-}
+        [Parameter(Mandatory=$True)]
+        [String]$Content,
 
-#* ------ Main ------
-if (Test-Path "$PSScriptRoot/services.json" -PathType Leaf) {
+        [Parameter(Mandatory=$True)]
+        [String]$FilePath
+    )
+
+    "[$(Get-Date -format "yyyy-MM-dd HH:mm:ss")] $Content" | Out-File $FilePath -Append
+}
+#Endregion
+
+
+
+#Region Main
+if (Test-Path $SnapshotFilePath -PathType Leaf) {
     $code = Start-Checking
 }
 else {
-    New-Config
+    New-Snapshot $SnapshotFilePath
     $code = Start-Checking
 }
-
 exit $code
+#Endregion
